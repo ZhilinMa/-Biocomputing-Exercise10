979fdb05d50dfaccde538b5e762c4166873c676a
Anna McCartan <amccarta@nd.edu>
2022-11-20 16:36:16 -0500

    Completed part 1 basketball analysis

--- McCartanExercise10.R
+++ McCartanExercise10.R
@@ -5,7 +5,7 @@ setwd("~/Library/CloudStorage/GoogleDrive-annamac2021@gmail.com/My Drive/School/
 gameData=read.table("UWvMSU_1-22-13.txt", header=TRUE)
 # generate df w cumulative score for each team
 scoredf=data.frame(matrix(nrow=nrow(gameData), ncol=3))
-colnames(scoredf)=c("time", "UW", "MSU")
+colnames(scoredf)=c("time", "team", "totalscore")
 # create cumulative score variables
 UWscore=0
 MSUscore=0
@@ -14,21 +14,30 @@ for (i in 1:nrow(gameData)){
   # if team = "UW," add score to UW's cumulative score
   if (gameData$team[i]=="UW"){
     UWscore=UWscore+gameData$score[i]
+    # add UW's cumulative score at this time to new df
+    scoredf$totalscore[i]=UWscore
   }
   # if team = "MSU," add score to UW's cumulative score
   else if (gameData$team[i]=="MSU"){
     MSUscore=MSUscore+gameData$score[i]
+    # add MSU's cumulative score at this time to new df
+    scoredf$totalscore[i]=MSUscore
   }
   # otherwise, return an error
   else{
     print("invalid team value")
   }
-  # add each team's cumulative score at this time to new df
+  # add time and team to new df
   scoredf$time[i]=gameData$time[i]
-  scoredf$UW[i]=UWscore
-  scoredf$MSU[i]=MSUscore
+  scoredf$team[i]=gameData$team[i]
 }
 # create summary line graph
+library(ggplot2)
+ggplot(scoredf,
+       aes(x=time, y=totalscore)) +
+  geom_line(aes(color=team)) +
+  ylab("score") +
+  theme_classic()
 
 # Part 2 guess my number
 # create vector of integers 1-100
